7692a27d90d5c2ae7f567d9ac8968e3e534cd6b0
Praem <samuelantunes@hotmail.fr>
2016-12-15 12:30:48 +0100

    Fonctions utiles de base

--- themes/montheme/function.php
+++ themes/montheme/function.php
@@ -1,30 +1,59 @@
 <?php
-
-function my_script_styles()
-{
-    wp_enqueue_styles('bootstrap-css', get_template_directory_uri().'/statics/css/bootstrap.min.css');
-    wp_enqueue_styles('main_style', get_stylesheet_uri());
-    wp_enqueue_styles('bootstrap-js', get_template_directory_uri().'/statics/js/bootstrap.min.js');
+/* Enqueue Styles and Scripts */
+function my_scripts_styles() {
+    wp_enqueue_style('boostrap-css', get_template_directory_uri().'/css/bootstrap.min.css');
+    wp_enqueue_style('main_style', get_stylesheet_uri());
+    wp_enqueue_script('bootstrap-js', get_template_directory_uri().'/js/bootstrap.min.js');
 }
 
 add_action('wp_enqueue_scripts', 'my_scripts_styles');
 
 /* Navigation */
-function my_menus()
-{
-    register_nav_menu('main_menu', 'Menu principal');
-    register_nav_menu('footer_menu', 'Menu de pied de page');
+function my_menus() {
+    register_nav_menu('main_menu', 'Menu Principal');
+    register_nav_menu('footer_menu', 'Menu Pied de page');
 }
 
 add_action('init', 'my_menus');
 
-function mySidebars()
-{
-    register_siderbar([
-        'name' => 'Barre latérale',
-        'description' => 'Cette colonne s\'affiche sur toutes les pages',
-        'id' => 'sidebar-1'
-    ]);
+/* Widget zones */
+
+function my_sidebars() {
+    register_sidebar([
+                        'id'          => 'sidebar-1' , 
+                        'description' => 'S\'affiche sur toutes les pages', 
+                        'name'        => 'Barre latérale'
+                    ]);
 }
 
-add_action('widgets_init', 'mySidebars');
+add_action('widgets_init', 'my_sidebars');
+
+/* En-tête */ 
+add_theme_support('custom-header');
+
+/* Image à la une */
+add_theme_support('post-thumbnails');
+
+/* Font personnalisé */
+add_theme_support('custom-background');
+
+/* Custom thumbnail size */
+add_action('after_setup_theme', 'wpdocs_theme_setup');
+
+function wpdocs_theme_setup() {
+    add_image_size('banner', 1240, 150, true);
+}
+
+/* Custom menu (onglet) */
+add_action('init', 'custom_post_type');
+
+function custom_post_type() {
+    register_post_type('Events',
+        [   
+            'labels' => [
+                'name' => __('Events'),
+                'singular_name' => __('Event')
+                ],
+            'public' => true
+        ]);
+}
