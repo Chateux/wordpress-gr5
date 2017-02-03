a2c38a0c8e09f5646b54d51702387ad579fb7e2f
Samuel <samuelantunes@hotmail.fr>
2017-02-03 12:33:34 +0100

    [widget-best-recipes] Add of class with form admin

--- themes/jecreveladalle/Objets/BestRecipes.php
+++ themes/jecreveladalle/Objets/BestRecipes.php
@@ -1,19 +1,30 @@
 <?php
-/*
-class BestRecipes extends WP_Widget {
-	function BestRecipes() {
-		// Instantate the parent boject
+
+class BestRecipes_Widget extends WP_Widget {
+
+	public function BestRecipes() {
+		parent::__construct('BestRecipes', 'Meilleures Recettes', ['description' => 'Les meilleures recettes.']);
 	}
 
-	function widget($args, $d) {
-		// Output
+	public function widget($args, $values) {
+		echo $args['before_widget'];
+		echo $args['before_title'];
+		echo apply_filters('widget_title', $values['title']);
+		echo $args['after_title'];
+		// @adrien - Ajout du template CSS ici
+		echo $args['after_widget'];
 	}
 
-	function update($new, $old) {
+	public function update($new, $old) {
 		// Save widget options
 	}
 
-	function form($d) {
-		// Output admin widget options from
+	public function form($values) {
+    $title = isset($instance['title']) ? $instance['title'] : '';
+    echo "
+    <p>
+        <label for=".echo $this->get_field_name( 'title' );.">". _e( 'Title:' ); ."</label>
+        <input class='widefat' id='". echo $this->get_field_id( 'title' ); ."' name='". echo $this->get_field_name( 'title' ); ."' type='text' value='". echo  $title; ."' />
+    </p>";
 	}
-}*/
+}
