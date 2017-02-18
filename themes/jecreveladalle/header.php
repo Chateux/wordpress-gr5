f02c44d8c88485b7f08ddc6f423f6fdf53b72a8e
apayen <apayen@arbouin-consulting.com>
2017-02-18 14:42:04 +0100

    update design

--- themes/jecreveladalle/header.php
+++ themes/jecreveladalle/header.php
@@ -2,6 +2,9 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?php wp_title(); echo ' - '; bloginfo('name'); ?></title>
     <?php wp_head(); ?>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -20,10 +23,11 @@
             <a href="<?php bloginfo('url'); ?>" class="navbar-brand"><?php bloginfo('name'); ?></a>
         </div>
 
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <div class="collapse navbar-collapse">
             <form class="navbar-form navbar-left">
-                <div class="form-group">
-                    <input type="text" class="form-control" placeholder="Search">
+                <div class="form-group search-bar">
+                    <button type="submit"><i class="fa fa-search" aria-hidden="true"></i></button>
+                    <input type="search" class="form-control" placeholder="Search">
                 </div>
             </form>
 
@@ -32,3 +36,34 @@
         </div>
     </div>
 </nav>
+
+<?php
+
+$loop = new WP_Query( array( 'post_type' => 'Evenement', 'posts_per_page' => 4 ) );
+
+?>
+<div class="carousel slide" data-ride="carousel">
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" role="listbox">
+        <?php
+        $first = true;
+        while ( $loop->have_posts() ) : $loop->the_post();
+        ?>
+
+        <?php if($first) { ?>
+        <div class="item active">
+            <?php $first = false; } else { ?>
+            <div class="item">
+                <?php } ?>
+
+                <?php the_post_thumbnail(); ?>
+                <div class="carousel-caption">
+                    <?php the_title(); ?>
+                </div>
+            </div>
+            <?php
+            endwhile;
+            ?>
+        </div>
+    </div>
+</div>
--- themes/jecreveladalle/single-recettes.php
+++ themes/jecreveladalle/single-recettes.php
@@ -1,21 +1,45 @@
 <?php get_header(); ?>
 
 <div class="container">
-	<?php if (have_posts()) : ?>
-		<?php while (have_posts()) : the_post(); ?>
+	<div class="row">
+		<div class="col-md-9">
+			<?php if (have_posts()) : ?>
+				<?php while (have_posts()) : the_post(); ?>
+					<article class="recipe">
+						<h1 class="title">
+							<?php the_title(); ?>
+						</h1>
 
-            <article class="recipe">
-                <h1 class="title">
-					<?php the_title(); ?>
-                </h1>
+						<div class="content">
+							<div class="short-desc row">
+								<div class="col-md-4">
+									<i class="fa fa-clock-o" aria-hidden="true"></i>
+									Cooking time :
+									<strong>15 min</strong>
+								</div>
+								<div class="col-md-4 txt-center">
+									<i class="fa fa-clock-o" aria-hidden="true"></i>
+									Difficulté :
+									<strong>1/5 ~ Easy</strong>
+								</div>
+								<div class="col-md-4 txt-right">
+									<i class="fa fa-star" aria-hidden="true"></i>
+									<i class="fa fa-star" aria-hidden="true"></i>
+									<i class="fa fa-star" aria-hidden="true"></i>
+									<i class="fa fa-star-o" aria-hidden="true"></i>
+									<i class="fa fa-star-o" aria-hidden="true"></i>
+								</div>
+							</div>
+							<div class="desc">
+								<?php the_content(); ?>
+							</div>
+						</div>
+					</article>
 
-                <div class="content">
-					<?php the_content(); ?>
-                </div>
-            </article>
-
-        <?php endwhile; ?>
-	<?php endif; ?>
+				<?php endwhile; ?>
+			<?php endif; ?>
+		</div>
+	</div>
 </div>
 
 <?php get_footer(); ?>
