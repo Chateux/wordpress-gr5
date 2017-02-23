4edec572d5a0bc1342b439c6ae6fd5436f454182
Samuel <samuelantunes@hotmail.fr>
2017-02-23 19:27:26 +0100

    [widget-social-network] to merge

--- themes/jecreveladalle/index.php
+++ themes/jecreveladalle/index.php
@@ -10,7 +10,7 @@ if (have_posts()) {
     }
 } else {
     echo "Aucun articles ...";
-}
+} 
 $loop = new WP_Query( array( 'post_type' => 'Evenement', 'posts_per_page' => 10 ) );
 while ( $loop->have_posts() ) : $loop->the_post();
     the_title();
